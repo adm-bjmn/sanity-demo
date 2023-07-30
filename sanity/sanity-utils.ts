0d0fca8ba2ae79770f9090bc3b9b50547253ced3
adm.bjmn <adm.bjmn@gmail.com>
2023-07-30 16:33:35 +0100

    all done :)

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -1,5 +1,13 @@
 import { Project } from "@/types/project";
+import { Page } from "@/types/page";
 import { createClient, groq } from "next-sanity";
+
+
+const clientConfig = {
+    projectId: 'vcna9kkd',
+    dataset: 'production',
+    apiVersion: '2023-07-28',
+};
 
 export async function getProjects(): Promise<Project[]> {
 
@@ -49,4 +57,36 @@ export async function getProject(slug: string): Promise<Project> {
         }`,
         { slug }
     );
+}
+
+export async function getPages(): Promise<Page[]> {
+
+
+    return createClient(clientConfig).fetch(
+
+        groq`*[_type == "page"]{
+        _id,
+        _createdAt,
+        title,
+        "slug": slug.current,
+    }`
+
+    )
+}
+
+export async function getPage(slug: string): Promise<Page> {
+
+    return createClient(clientConfig).fetch(
+
+        groq`*[_type == "page" && slug.current == $slug][0]{
+        _id,
+        _createdAt,
+        title,
+        "slug": slug.current,
+        content
+    }`,
+        { slug },
+
+    )
+
 }
